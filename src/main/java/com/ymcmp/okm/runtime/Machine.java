0a22f70d270ed3d459573f485f49a0ccef4cd596
Paul Teng <plankp@outlook.com>
2018-09-13 09:18:28 -0400

    Update comments

--- src/main/java/com/ymcmp/okm/runtime/Machine.java
+++ src/main/java/com/ymcmp/okm/runtime/Machine.java
@@ -68,151 +68,151 @@ public class Machine {
             final Statement stmt = func.get(i);
             try {
                 switch (stmt.op) {
-                    case NOP:           // NOP              <ignore>
+                    case NOP:           //      <ignore>
                         // NOP does nothing..
                         break;
-                    case CONV_BYTE_INT: // CONV_BYTE_INT    dst:result, lhs:base
-                    case CONV_SHORT_INT:// CONV_SHORT_INT   dst:result, lhs:base
+                    case CONV_BYTE_INT: //      dst:result, lhs:base
+                    case CONV_SHORT_INT://      dst:result, lhs:base
                         locals.put(stmt.dst, new Fixnum(toInt(fetchValue(stmt.lhs)), Integer.SIZE));
                         break;
-                    case CONV_LONG_INT: // CONV_LONG_INT    dst:result, lhs:base
+                    case CONV_LONG_INT: //      dst:result, lhs:base
                         locals.put(stmt.dst, new Fixnum(toLong(fetchValue(stmt.lhs)), Integer.SIZE));
                         break;
-                    case CONV_INT_BYTE: // CONV_INT_BYTE    dst:result, lhs:base
+                    case CONV_INT_BYTE: //      dst:result, lhs:base
                         locals.put(stmt.dst, new Fixnum(toInt(fetchValue(stmt.lhs)), Byte.SIZE));
                         break;
-                    case CONV_INT_SHORT:// CONV_INT_SHORT   dst:result, lhs:base
+                    case CONV_INT_SHORT://      dst:result, lhs:base
                         locals.put(stmt.dst, new Fixnum(toInt(fetchValue(stmt.lhs)), Short.SIZE));
                         break;
-                    case CONV_INT_LONG: // CONV_INT_LONG    dst:result, lhs:base
+                    case CONV_INT_LONG: //      dst:result, lhs:base
                         locals.put(stmt.dst, new Fixnum(toLong(fetchValue(stmt.lhs))));
                         break;
-                    case CONV_INT_DOUBLE: //                dst:result, lhs:base
-                    case CONV_LONG_DOUBLE: //               dst:result, lhs:base
-                    case CONV_FLOAT_DOUBLE: //              dst:result, lhs:base
+                    case CONV_INT_DOUBLE: //    dst:result, lhs:base
+                    case CONV_LONG_DOUBLE: //   dst:result, lhs:base
+                    case CONV_FLOAT_DOUBLE: //  dst:result, lhs:base
                         locals.put(stmt.dst, new Fixnum(toDouble(fetchValue(stmt.lhs))));
                         break;
-                    case INT_LT:        // INT_LT           dst:result, lhs:a, rhs:b
+                    case INT_LT:        //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, makeBool(toInt(fetchValue(stmt.lhs)) < toInt(fetchValue(stmt.rhs))));
                         break;
-                    case INT_GT:        // INT_GT           dst:result, lhs:a, rhs:b
+                    case INT_GT:        //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, makeBool(toInt(fetchValue(stmt.lhs)) > toInt(fetchValue(stmt.rhs))));
                         break;
-                    case INT_LE:        // INT_LE           dst:result, lhs:a, rhs:b
+                    case INT_LE:        //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, makeBool(toInt(fetchValue(stmt.lhs)) <= toInt(fetchValue(stmt.rhs))));
                         break;
-                    case INT_GE:        // INT_GE           dst:result, lhs:a, rhs:b
+                    case INT_GE:        //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, makeBool(toInt(fetchValue(stmt.lhs)) >= toInt(fetchValue(stmt.rhs))));
                         break;
-                    case INT_EQ:        // INT_EQ           dst:result, lhs:a, rhs:b
+                    case INT_EQ:        //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, makeBool(toInt(fetchValue(stmt.lhs)) == toInt(fetchValue(stmt.rhs))));
                         break;
-                    case INT_NE:        // INT_NE           dst:result, lhs:a, rhs:b
+                    case INT_NE:        //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, makeBool(toInt(fetchValue(stmt.lhs)) != toInt(fetchValue(stmt.rhs))));
                         break;
-                    case INT_NEG:       // INT_NEG          dst:result, lhs:base
+                    case INT_NEG:       //      dst:result, lhs:base
                         locals.put(stmt.dst, new Fixnum(-toInt(fetchValue(stmt.lhs)), Integer.SIZE));
                         break;
-                    case INT_CPL:       // INT_CPL          dst:result, lhs:base
+                    case INT_CPL:       //      dst:result, lhs:base
                         locals.put(stmt.dst, new Fixnum(~toInt(fetchValue(stmt.lhs)), Integer.SIZE));
                         break;
-                    case INT_ADD:       // INT_ADD          dst:result, lhs:a, rhs:b
+                    case INT_ADD:       //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toInt(fetchValue(stmt.lhs)) + toInt(fetchValue(stmt.rhs)), Integer.SIZE));
                         break;
-                    case INT_SUB:       // INT_SUB          dst:result, lhs:a, rhs:b
+                    case INT_SUB:       //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toInt(fetchValue(stmt.lhs)) - toInt(fetchValue(stmt.rhs)), Integer.SIZE));
                         break;
-                    case INT_MUL:       // INT_MUL          dst:result, lhs:a, rhs:b
+                    case INT_MUL:       //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toInt(fetchValue(stmt.lhs)) * toInt(fetchValue(stmt.rhs)), Integer.SIZE));
                         break;
-                    case INT_DIV:       // INT_DIV          dst:result, lhs:a, rhs:b
+                    case INT_DIV:       //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toInt(fetchValue(stmt.lhs)) / toInt(fetchValue(stmt.rhs)), Integer.SIZE));
                         break;
-                    case INT_MOD:       // INT_MOD          dst:result, lhs:a, rhs:b
+                    case INT_MOD:       //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toInt(fetchValue(stmt.lhs)) % toInt(fetchValue(stmt.rhs)), Integer.SIZE));
                         break;
-                    case LONG_CMP:      // LONG_CMP         dst:result, lhs:a, rhs:b
+                    case LONG_CMP:      //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(Long.compare(toLong(fetchValue(stmt.lhs)), toLong(fetchValue(stmt.rhs))), Integer.SIZE));
                         break;
-                    case LONG_NEG:      // LONG_NEG         dst:result, lhs:base
+                    case LONG_NEG:      //      dst:result, lhs:base
                         locals.put(stmt.dst, new Fixnum(-toLong(fetchValue(stmt.lhs))));
                         break;
-                    case LONG_CPL:      // LONG_CPL         dst:result, lhs:base
+                    case LONG_CPL:      //      dst:result, lhs:base
                         locals.put(stmt.dst, new Fixnum(~toLong(fetchValue(stmt.lhs))));
                         break;
-                    case LONG_ADD:      // LONG_ADD         dst:result, lhs:a, rhs:b
+                    case LONG_ADD:      //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toLong(fetchValue(stmt.lhs)) + toLong(fetchValue(stmt.rhs))));
                         break;
-                    case LONG_SUB:      // LONG_SUB         dst:result, lhs:a, rhs:b
+                    case LONG_SUB:      //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toLong(fetchValue(stmt.lhs)) - toLong(fetchValue(stmt.rhs))));
                         break;
-                    case LONG_MUL:      // LONG_MUL         dst:result, lhs:a, rhs:b
+                    case LONG_MUL:      //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toLong(fetchValue(stmt.lhs)) * toLong(fetchValue(stmt.rhs))));
                         break;
-                    case LONG_DIV:      // LONG_DIV         dst:result, lhs:a, rhs:b
+                    case LONG_DIV:      //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toLong(fetchValue(stmt.lhs)) / toLong(fetchValue(stmt.rhs))));
                         break;
-                    case LONG_MOD:      // LONG_MOD         dst:result, lhs:a, rhs:b
+                    case LONG_MOD:      //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toLong(fetchValue(stmt.lhs)) % toLong(fetchValue(stmt.rhs))));
                         break;
-                    case FLOAT_CMP:     // FLOAT_CMP        dst:result, lhs:a, rhs:b
+                    case FLOAT_CMP:     //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(Float.compare(toFloat(fetchValue(stmt.lhs)), toFloat(fetchValue(stmt.rhs))), Integer.SIZE));
                         break;
-                    case FLOAT_NEG:     // FLOAT_NEG        dst:result, lhs:base
+                    case FLOAT_NEG:     //      dst:result, lhs:base
                         locals.put(stmt.dst, new Fixnum(-toFloat(fetchValue(stmt.lhs)), Float.SIZE));
                         break;
-                    case FLOAT_ADD:     // FLOAT_ADD        dst:result, lhs:a, rhs:b
+                    case FLOAT_ADD:     //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toFloat(fetchValue(stmt.lhs)) + toFloat(fetchValue(stmt.rhs)), Float.SIZE));
                         break;
-                    case FLOAT_SUB:     // FLOAT_SUB        dst:result, lhs:a, rhs:b
+                    case FLOAT_SUB:     //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toFloat(fetchValue(stmt.lhs)) - toFloat(fetchValue(stmt.rhs)), Float.SIZE));
                         break;
-                    case FLOAT_MUL:     // FLOAT_MUL        dst:result, lhs:a, rhs:b
+                    case FLOAT_MUL:     //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toFloat(fetchValue(stmt.lhs)) * toFloat(fetchValue(stmt.rhs)), Float.SIZE));
                         break;
-                    case FLOAT_DIV:     // FLOAT_DIV        dst:result, lhs:a, rhs:b
+                    case FLOAT_DIV:     //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toFloat(fetchValue(stmt.lhs)) / toFloat(fetchValue(stmt.rhs)), Float.SIZE));
                         break;
-                    case FLOAT_MOD:     // FLOAT_MOD        dst:result, lhs:a, rhs:b
+                    case FLOAT_MOD:     //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toFloat(fetchValue(stmt.lhs)) % toFloat(fetchValue(stmt.rhs)), Float.SIZE));
                         break;
-                    case DOUBLE_CMP:    // DOUBLE_CMP       dst:result, lhs:a, rhs:b
+                    case DOUBLE_CMP:    //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(Double.compare(toDouble(fetchValue(stmt.lhs)), toDouble(fetchValue(stmt.rhs))), Integer.SIZE));
                         break;
-                    case DOUBLE_NEG:    // DOUBLE_NEG       dst:result, lhs:base
+                    case DOUBLE_NEG:    //      dst:result, lhs:base
                         locals.put(stmt.dst, new Fixnum(-toDouble(fetchValue(stmt.lhs))));
                         break;
-                    case DOUBLE_ADD:    // DOUBLE_ADD       dst:result, lhs:a, rhs:b
+                    case DOUBLE_ADD:    //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toDouble(fetchValue(stmt.lhs)) + toDouble(fetchValue(stmt.rhs))));
                         break;
-                    case DOUBLE_SUB:    // DOUBLE_SUB       dst:result, lhs:a, rhs:b
+                    case DOUBLE_SUB:    //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toDouble(fetchValue(stmt.lhs)) - toDouble(fetchValue(stmt.rhs))));
                         break;
-                    case DOUBLE_MUL:    // DOUBLE_MUL       dst:result, lhs:a, rhs:b
+                    case DOUBLE_MUL:    //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toDouble(fetchValue(stmt.lhs)) * toDouble(fetchValue(stmt.rhs))));
                         break;
-                    case DOUBLE_DIV:    // DOUBLE_DIV       dst:result, lhs:a, rhs:b
+                    case DOUBLE_DIV:    //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toDouble(fetchValue(stmt.lhs)) / toDouble(fetchValue(stmt.rhs))));
                         break;
-                    case DOUBLE_MOD:    // DOUBLE_MOD       dst:result, lhs:a, rhs:b
+                    case DOUBLE_MOD:    //      dst:result, lhs:a, rhs:b
                         locals.put(stmt.dst, new Fixnum(toDouble(fetchValue(stmt.lhs)) % toDouble(fetchValue(stmt.rhs))));
                         break;
-                    case LOAD_TRUE:     // LOAD_TRUE        dst:result
+                    case LOAD_TRUE:     //      dst:result
                         locals.put(stmt.dst, Fixnum.TRUE);
                         break;
-                    case LOAD_FALSE:    // LOAD_FALSE       dst:result
+                    case LOAD_FALSE:    //      dst:result
                         locals.put(stmt.dst, Fixnum.FALSE);
                         break;
-                    case LOAD_NUMERAL:  // LOAD_NUMERAL     dst:result, lhs:value
+                    case LOAD_NUMERAL:  //      dst:result, lhs:value
                         locals.put(stmt.dst, stmt.lhs);
                         break;
-                    case STORE_VAR:     // STORE_VAR        dst:store, lhs:value
+                    case STORE_VAR:     //      dst:store, lhs:value
                         locals.put(stmt.dst, fetchValue(stmt.lhs).duplicate());
                         break;
-                    case REFER_VAR:     // REFER_VAR        dst:store, lhs:register
+                    case REFER_VAR:     //      dst:store, lhs:register
                         locals.put(stmt.dst, new MutableCell(fetchValue(stmt.lhs)));
                         break;
-                    case REFER_ATTR: {  // REFER_ATTR       dst:store, lhs:struct, rhs:attr
+                    case REFER_ATTR: {  //      dst:store, lhs:struct, rhs:attr
                         final StructFields struct = (StructFields) fetchValue(stmt.lhs);
                         final String attr = stmt.rhs.toString();
                         locals.put(stmt.dst, new Mutable() {
@@ -234,55 +234,55 @@ public class Machine {
                         });
                         break;
                     }
-                    case POINTER_GET:   // POINTER_GET      dst:store, lhs:pointer
+                    case POINTER_GET:   //      dst:store, lhs:pointer
                         locals.put(stmt.dst, ((Mutable) fetchValue(stmt.lhs)).getValue());
                         break;
-                    case POINTER_PUT:   // POINTER_PUT      dst:pointer, lhs:value
+                    case POINTER_PUT:   //      dst:pointer, lhs:value
                         ((Mutable) fetchValue(stmt.dst)).setValue(fetchValue(stmt.lhs));
                         break;
-                    case DEREF_GET_ATTR: //                 dst:store, lhs:pointer to struct, rhs:attr
+                    case DEREF_GET_ATTR: //     dst:store, lhs:pointer to struct, rhs:attr
                         locals.put(stmt.dst, ((StructFields) ((Mutable) fetchValue(stmt.lhs)).getValue()).get(stmt.rhs.toString()));
                         break;
-                    case DEREF_PUT_ATTR: //                 dst:value, lhs:pointer to struct, rhs:attr
+                    case DEREF_PUT_ATTR: //     dst:value, lhs:pointer to struct, rhs:attr
                         ((StructFields) ((Mutable) fetchValue(stmt.lhs)).getValue()).put(stmt.rhs.toString(), fetchValue(stmt.dst));
                         break;
-                    case ALLOC_STRUCT:  // ALLOC_STRUCT     dst:store, lhs:size of struct (we ignore this)
+                    case ALLOC_STRUCT:  //      dst:store, lhs:size of struct (we ignore this)
                         locals.put(stmt.dst, new StructFields());
                         break;
-                    case GET_ATTR:      // GET_ATTR         dst:store, lhs:struct, rhs:attr
+                    case GET_ATTR:      //      dst:store, lhs:struct, rhs:attr
                         locals.put(stmt.dst, ((StructFields) fetchValue(stmt.lhs)).get(stmt.rhs.toString()));
                         break;
-                    case PUT_ATTR:      // PUT_ATTR         dst:value, lhs:struct, rhs:attr
+                    case PUT_ATTR:      //      dst:value, lhs:struct, rhs:attr
                         ((StructFields) fetchValue(stmt.lhs)).put(stmt.rhs.toString(), fetchValue(stmt.dst));
                         break;
-                    case RETURN_UNIT:   // RETURN_UNIT
+                    case RETURN_UNIT:   //      <ignore>
                         return null;
-                    case RETURN_VALUE:  // RETURN_VALUE     dst:result
+                    case RETURN_VALUE:  //      dst:result
                         return fetchValue(stmt.dst);
-                    case GOTO:          // GOTO             dst:jumpsite
+                    case GOTO:          //      dst:jumpsite
                         i = ((Label) stmt.dst).getAddress() - 1;    // -1 because loop invariant
                         break;
-                    case JUMP_IF_TRUE:  // JUMP_IF_TRUE     dst:jumpsite, lhs:value
+                    case JUMP_IF_TRUE:  //      dst:jumpsite, lhs:value
                         if (toInt(fetchValue(stmt.lhs)) != 0) {
                             i = ((Label) stmt.dst).getAddress() - 1;    // -1 because loop invariant
                         }
                         break;
-                    case JUMP_IF_FALSE: // JUMP_IF_FALSE    dst:jumpsite, lhs:value
+                    case JUMP_IF_FALSE: //      dst:jumpsite, lhs:value
                         if (toInt(fetchValue(stmt.lhs)) == 0) {
                             i = ((Label) stmt.dst).getAddress() - 1;    // -1 because loop invariant
                         }
                         break;
-                    case POP_PARAM:     // POP_PARAM        dst:store
+                    case POP_PARAM:     //      dst:store
                         locals.put(stmt.dst, callStack.pop());
                         break;
-                    case PUSH_PARAM:    // PUSH_PARAM       dst:value
+                    case PUSH_PARAM:    //      dst:value
                         // Pass by value, (including structs)
                         callStack.push(fetchValue(stmt.dst).duplicate());
                         break;
-                    case CALL:          // CALL             dst:store, lhs:callsite
+                    case CALL:          //      dst:store, lhs:callsite
                         locals.put(stmt.dst, execute(code, fetchValue(stmt.lhs).toString()));
                         break;
-                    case TAILCALL: {    // TAILCALL         dst:callsite
+                    case TAILCALL: {    //      dst:callsite
                         // Update parameter $list, reset counter $i and restart
                         // unless it is one of the special functions
                         final String funcName = stmt.dst.toString();
